959bd2e8ac24aac0dca3b58c082a13be79de58af
Uoo-Kay-Mech <sh.computerhealthcare@gmail.com>
2026-06-28 21:43:25 +0530

    fix: accessibility ARIA‑hidden focus bug & preload warnings

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,8 @@ const cspHeader = `
   style-src 'self' 'unsafe-inline';
   img-src 'self' blob: data: https://uookaymechindustries.com;
   font-src 'self' data:;
-  frame-src 'self' https://maps.google.com https://www.google.com;
+  frame-src 'self' https://*.google.com https://*.google.co.in;
+  child-src 'self' https://*.google.com https://*.google.co.in;
   object-src 'none';
   base-uri 'self';
   form-action 'self';
